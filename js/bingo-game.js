b2349b8cf7e63152a56893c53d5224373559cf2e
Jordan Nicholson <jordannicholson@Jordans-MacBook-Pro.local>
2019-12-21 21:50:32 +0000

    Included a console.log so we can track the numbers that have come up

--- js/bingo-game.js
+++ js/bingo-game.js
@@ -1,4 +1,5 @@
 function myFunction() {
   var numberGenerator = document.getElementById("bingo-number")
   numberGenerator.innerHTML = Math.floor((Math.random() * 90) + 1);
+  console.log(numberGenerator);
 }
